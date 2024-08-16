dfaa932cff140042cd1804dcddf59b14e8adb0d3
Shravan Agrawal <shravan393@gmail.com>
2024-08-16 08:07:30 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DogPicsLibrary",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DogPicsLibrary",
+            targets: ["DogPicsLibrary"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DogPicsLibrary"),
+        .testTarget(
+            name: "DogPicsLibraryTests",
+            dependencies: ["DogPicsLibrary"]),
+    ]
+)
